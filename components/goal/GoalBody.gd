2f608f54a8094f2669c6e4f61a00dc66e55efb72
Bundeskranzler <62666483+Bundeskranzler@users.noreply.github.com>
2022-06-13 22:14:46 +0200

    Level2

--- components/goal/GoalBody.gd
+++ components/goal/GoalBody.gd
@@ -13,7 +13,9 @@ extends Area2D
 #	pass
 
 func _on_GoalBody_body_entered(body):
-	if get_parent().modulate.is_equal_approx(SceneSkript.colors.white) or get_parent().modulate.is_equal_approx(body.get_parent().modulate):
+	if get_parent().color.is_equal_approx(SceneSkript.colors.white) or get_parent().color.is_equal_approx(body.get_parent().modulate):
+		var a= get_parent().modulate
+		var b= body.get_parent().modulate
 		body.delete_crate()
 		get_parent().reduce_count()
 	else:
